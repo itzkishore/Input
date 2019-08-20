309251973d927aec8ba79119eef82c78c946fd38
itzkishore <54090619+itzkishore@users.noreply.github.com>
2019-08-20 10:45:48 +0530

    Create New.java

---
+++ New.java
@@ -0,0 +1,14 @@
+import java.util.*;  
+
+ class Akshay {
+ public static void main(String args[]){
+System.out.println("Hello World");
+Scanner in = new Scanner(System.in);  
+          System.out.print("Enter your name: ");  
+          String name = in.nextLine();  
+          System.out.println("Name is: " + name); 
+      in.close();     
+     
+ }
+}
+
